4e4734ed95498b89010f0ceb4adc0f2b3737d1a0
Atharva.Thakur <thakur27@purdue.edu>
2023-06-19 14:34:29 +0530

    temp fix

--- downloader.py
+++ downloader.py
@@ -1,9 +1,16 @@
 from pytube import YouTube
+import moviepy.editor as mp
+import os
 
 video_url = "https://www.youtube.com/watch?v=xtoHuHgS9_o&pp=ygUKZGFyayBob3JzZQ%3D%3D"
 yt = YouTube(video_url)
+title = yt.title
+print(title)
+streams = yt.streams.filter(only_audio=True)
+stream = streams[-1]  # Choose the first stream
 
-streams = yt.streams
-stream = streams[0]  # Choose the first stream
+temp_filename = stream.download()  # Download the video
+mp_audio = mp.AudioFileClip(temp_filename)
+mp_audio.write_audiofile(f"{title}.mp3")
 
-stream.download()  # Download the video
+os.remove(temp_filename)
