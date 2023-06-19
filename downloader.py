70707c95a9d8c1b32aa6579780954ad531d37195
Atharva.Thakur <thakur27@purdue.edu>
2023-06-19 14:34:29 +0530

    enjoy music

--- downloader.py
+++ downloader.py
@@ -1,1 +1,9 @@
-import pytube
+from pytube import YouTube
+
+video_url = "https://www.youtube.com/watch?v=xtoHuHgS9_o&pp=ygUKZGFyayBob3JzZQ%3D%3D"
+yt = YouTube(video_url)
+
+streams = yt.streams
+stream = streams[0]  # Choose the first stream
+
+stream.download()  # Download the video
