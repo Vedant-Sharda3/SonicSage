a2319bf3fd97183c68900900d279311c90f40f9c
Vedant-Sharda3 <vsharda313@gmail.com>
2023-06-28 13:01:11 +0530

    TesterPlaylist amended to fix autoplay issue.

--- Backend/TesterPlaylist.py
+++ Backend/TesterPlaylist.py
@@ -4,46 +4,53 @@ import os
 pygame.init()
 pygame.mixer.init()
 
+
 songs_directory = "songs_mp3"
 song_files = os.listdir(songs_directory)
 playlist = []
 current_song_index = 0
 is_playing = False
 
+# Create playlist
 for song_file in song_files:
     song_path = os.path.join(songs_directory, song_file)
     playlist.append(song_path)
 
+# Function to play the current song
 def play_song():
     global is_playing
     pygame.mixer.music.load(playlist[current_song_index])
     pygame.mixer.music.play()
     is_playing = True
 
+# Function to pause the current song
 def pause_song():
     global is_playing
     pygame.mixer.music.pause()
     is_playing = False
 
+# Function to resume the current song
 def resume_song():
     global is_playing
     pygame.mixer.music.unpause()
     is_playing = True
 
+# Function to play the next song
 def next_song():
     global current_song_index
-    if current_song_index < len(playlist) - 1:
-        current_song_index += 1
-    else:
-        current_song_index = 0
+    current_song_index = (current_song_index + 1) % len(playlist)
     pygame.mixer.music.stop()
     play_song()
 
+screen = pygame.display.set_mode((400, 300))
+
+# Play the first song
 play_song()
 
-screen = pygame.display.set_mode((400, 300))
+# Game loop
 running = True
 while running:
+    # Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -56,6 +63,13 @@ while running:
             elif event.key == pygame.K_RIGHT:
                 next_song()
 
+    # Check if the current song has finished playing
+    if not pygame.mixer.music.get_busy() and is_playing:
+        next_song()
+
+    # Add a small delay to reduce CPU usage
     pygame.time.delay(100)
 
 pygame.quit()
+
+
